0b448b0b17639394802dc367d44407a42afa2e4a
atroberg <alexistroberg@gmail.com>
2014-04-02 14:11:11 +0200

    Yeoman skeleton

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('apps4europeAdminInterfaceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
