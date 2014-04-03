448d0c893d13b8000275961a3d514856879af364
atroberg <alexistroberg@gmail.com>
2014-04-03 22:06:25 +0200

    Easen testing of app by automatically fixing URL:s

---
+++ app/scripts/debug.js
@@ -0,0 +1,16 @@
+/*global $:false */
+
+'use strict';
+
+// Tmp fix for links
+$(document).on('ready', function() {
+  $('body').on('click', 'a', function() {
+    var $el = $(this);
+
+    // Test for relative links
+    var href = $el.attr('href');
+    if ( href.indexOf('//') === -1 && href.indexOf('/#/') === -1 ) {
+      $el.attr('href', '/#' + href);
+    }
+  });
+});
